65756f59040f73c615090a9069016f2eb568cad8
root <root@default.hostname>
2015-08-17 05:15:36 -0400

    添加登录API的demo

---
+++ login.php
@@ -0,0 +1,24 @@
+<html>
+<head>
+<metahttp-equiv="content-type"content="text/html;charset=utf-8">
+</head>
+<h1>管理员登陆系统</h1>
+<formaction="loginprocess.php"method="post">
+<table>
+<tr><td>用户id</td><td><inputtype="text"name="id"/></td></tr>
+<tr><td>密&nbsp;码</td><td><inputtype="password"name="password"/></td></tr>
+<tr>
+<td><inputtype="submit"value="用户登录"/></td>
+<td><inputtype="reset"value="重新填写"/></td>
+</tr>
+</table>
+</form>
+<?php
+if(!empty($_GET['errno'])){
+$errno=$_GET['errno'];
+if($errno=='1'){
+echo "<br/><font color='red' size='3'>你的用户名和密码有误";
+}
+}
+?>
+</html>
