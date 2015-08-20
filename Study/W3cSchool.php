207db3d0cce53f44b6fe1f72d4a2f170dc9068e0
Alex D <Alex@Alex.local>
2015-08-20 17:12:03 +0800

    cm

--- Study/W3cSchool.php
+++ Study/W3cSchool.php
@@ -1,33 +1,33 @@
 <?php
-$txt="Hello World\n";
-echo $txt;
-?>
+    $txt="Hello World\n";
+    echo $txt;
+    ?>
 
 
 <?php
-$txt1="Hello World";
-$txt2="1234\n";
-echo $txt1 . " " . $txt2;
-?>
+    $txt1="Hello World";
+    $txt2="1234\n";
+    echo $txt1 . " " . $txt2;
+    ?>
 
 <?php
-echo strlen("Hello world!\n");
-?>
+    echo strlen("Hello world!\n");
+    ?>
 
 <?php
-echo strpos("Hello world!","world");
-?> 
+    echo strpos("Hello world!","world");
+    ?>
 
 
 <html>
 <body>
 <?php
-$d=date("D");
-if ($d=="Fri")
-  echo "Have a nice weekend!\n"; 
-else
-  echo "Have a nice day!\n"; 
-?>
+    $d=date("D");
+    if ($d=="Fri")
+    echo "Have a nice weekend!\n";
+    else
+    echo "Have a nice day!\n";
+    ?>
 </body>
 </html>
 
@@ -35,14 +35,14 @@ else
 <html>
 <body>
 <?php
-$d=date("D");
-if ($d=="Fri")
-  {
-  echo "Hello!<br />"; 
-  echo "Have a nice weekend!\n";
-  echo "See you on Monday!\n";
-  }
-?>
+    $d=date("D");
+    if ($d=="Fri")
+    {
+        echo "Hello!<br />";
+        echo "Have a nice weekend!\n";
+        echo "See you on Monday!\n";
+    }
+    ?>
 </body>
 </html>
 
@@ -50,42 +50,42 @@ if ($d=="Fri")
 <html>
 <body>
 <?php
-$d=date("D");
-if ($d=="Fri")
-  echo "Have a nice weekend!\n"; 
-elseif ($d=="Sun")
-  echo "Have a nice Sunday!\n"; 
-else
-  echo "Have a nice day!\n"; 
-?>
+    $d=date("D");
+    if ($d=="Fri")
+    echo "Have a nice weekend!\n";
+    elseif ($d=="Sun")
+    echo "Have a nice Sunday!\n";
+    else
+    echo "Have a nice day!\n";
+    ?>
 </body>
 </html>
 
 
 <?php
-$x = 1;
-switch ($x) {
-case 1:
-  echo "Number 1\n";
-  break;
-case 2:
-  echo "Number 2\n";
-  break;
-case 3:
-  echo "Number 3\n";
-  break;
-default:
-  echo "No number between 1 and 3\n";
-}
-?>
+    $x = 1;
+    switch ($x) {
+        case 1:
+            echo "Number 1\n";
+            break;
+        case 2:
+            echo "Number 2\n";
+            break;
+        case 3:
+            echo "Number 3\n";
+            break;
+        default:
+            echo "No number between 1 and 3\n";
+    }
+    ?>
 
 
 
 <?php
-	$names = array("Peter","Quagmire","Joe");
-	$names[0] = "PeterNew";
-	echo $names[1] . " and " . $names[2] . " are ". $names[0] . "'s neighbors\n";
-?>
+    $names = array("Peter","Quagmire","Joe");
+    $names[0] = "PeterNew";
+    echo $names[1] . " and " . $names[2] . " are ". $names[0] . "'s neighbors\n";
+    ?>
 
 
 
--- Study/index.php
+++ Study/index.php
@@ -1,9 +1,9 @@
 <?php
-	$module = $_GET['module'];
-	$action = $_GET['action'];
-	$token = md5sum($module.date('Y-m-d',time()).'#$@%!*'.$action);
-	if($token != $_GET['token']){
-		alarm('access deny');
-		exit();
-		}
-?>
+    $module = $_GET['module'];
+    $action = $_GET['action'];
+    $token = md5sum($module.date('Y-m-d',time()).'#$@%!*'.$action);
+    if($token != $_GET['token']){
+        alarm('access deny');
+        exit();
+    }
+    ?>
