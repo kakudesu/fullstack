ebdcf2dc4cb3792c02b15cea33216682ee99fecc
root <root@default.hostname>
2015-08-20 03:52:28 -0400

    update

---
+++ Study/index.php
@@ -0,0 +1,9 @@
+<?php
+	$module = $_GET['module'];
+	$action = $_GET['action'];
+	$token = md5sum($module.date('Y-m-d',time()).'#$@%!*'.$action);
+	if($token != $_GET['token']){
+		alarm('access deny');
+		exit();
+		}
+?>
