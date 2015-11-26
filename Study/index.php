254f160c343bbc2f50e942d9e82d54a530e01165
DingSongWen <DingSongWen@haizhi.com>
2015-11-26 12:14:37 +0800

    ijijdeide

--- Study/index.php
+++
@@ -1,9 +0,0 @@
-<?php
-    $module = $_GET['module'];
-    $action = $_GET['action'];
-    $token = md5sum($module.date('Y-m-d',time()).'#$@%!*'.$action);
-    if($token != $_GET['token']){
-        alarm('access deny');
-        exit();
-    }
-    ?>
