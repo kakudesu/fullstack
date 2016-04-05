3159f834bacacbc3a5fe7905c0c58acbe571a309
DingSongWen <DingSongWen@haizhi.com>
2016-04-05 16:37:41 +0800

    cm test echo

--- index.php
+++ index.php
@@ -1,9 +1,9 @@
 <?php
 //重定向浏览器
-if (strcmp($_SERVER['HTTP_HOST'], "cuicui.dingsoung.com")) {
+if (strcmp($_SERVER['HTTP_HOST'], "http://cuicui.dingsoung.com")) {
         header("Location: http://dingsoung.com/cuicui/);
 } else {
-        exit;
+        echo $_SERVER['HTTP_HOST'];
 }
 
 //确保重定向后，后续代码不会被执行
