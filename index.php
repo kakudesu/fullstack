e4a93014e9569f3c5e37569da8f8e82bb798332e
DingSongWen <DingSongWen@haizhi.com>
2016-04-05 16:42:52 +0800

    cm

--- index.php
+++ index.php
@@ -1,11 +1,12 @@
 <?php
 //重定向浏览器
+//$_SERVER['HTTP_HOST']
+
 if (strcmp($_SERVER['HTTP_HOST'], "http://cuicui.dingsoung.com")) {
         header("Location: http://dingsoung.com/cuicui/);
 } else {
         echo $_SERVER['HTTP_HOST'];
 }
-
-//确保重定向后，后续代码不会被执行
 exit;
+
 ?>
