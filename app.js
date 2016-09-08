ff214215d3e6575a00c15ede051da1b8520eaf5e
Songwen Ding <DingSoung@gmail.com>
2016-09-08 22:11:35 +0800

    javascript 对象

--- app.js
+++ app.js
@@ -1,14 +1,40 @@
 function print(value) {
-    document.write(value);
+    document.writeln(value);
 }
+print('hello javascript');
 
-function clc(num, tang) {
-    return num * tang;
-}
+print('<br/>');
 
 function hello(deagree) {
     return Math.log(deagree);
 }
+print(hello(1));
 
-print(hello(4));
-print(clc(3, 4.00));
+print('<br/>');
+
+function totalPrice(num, tang) {
+    return num * tang;
+}
+print(totalPrice(4, 3));
+
+print('<br/>');
+product = {};
+product.price = 4.00;
+product.num = 3;
+print(totalPrice(product.price, product.num));
+
+print('<br/>');
+var IO = new Object();
+IO.print = print;
+IO.print("a obejct with function");
+IO.print(typeof IO.print);
+
+print('<br/>');
+var Person = { name: "phodal", weight: 50, height: 166 };
+
+function dream() {
+    future;
+};
+Person.future = dream;
+print(typeof Person)
+print(Person.future);
