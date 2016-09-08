ee9177382cb623124a88e0fdb9a4220db24b616f
Songwen Ding <DingSoung@gmail.com>
2016-09-08 20:32:09 +0800

    js  函数  设计

--- app.js
+++ app.js
@@ -1,1 +1,14 @@
-document.write(3 * 5 + 99889)
+function print(value) {
+    document.write(value);
+}
+
+function clc(num, tang) {
+    return num * tang;
+}
+
+function hello(deagree) {
+    return Math.log(deagree);
+}
+
+print(hello(4));
+print(clc(3, 4.00));
