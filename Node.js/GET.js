f13100e6b50b4c7aa30b059cceea42e9aec5f43e
Songwen Ding <DingSoung@gmail.com>
2016-11-07 15:02:18 +0800

    New post and get

--- Node.js/GET.js
+++ Node.js/GET.js
@@ -1,10 +1,7 @@
 var http = require('http');
 var url = require('url');
-var util = require('util');
-
-http.createServer(function(req, res){
-    console.log('app is listening http://localhost:3000/user?name=w3c&email=w3c@w3cschool.cc');
-    res.writeHead(200, {'Content-Type': 'text/plain'});
-    res.end(util.inspect(url.parse(req.url, true)));
+http.createServer(function(req, res) {
+    var reqult = url.parse(req.url, true)
+    res.writeHead(200, { 'Content-Type': 'text/plain' });
+    res.end(reqult);
 }).listen(3000);
-
--- Node.js/POST.js
+++ Node.js/POST.js
@@ -1,14 +1,18 @@
 var http = require('http');
 var querystring = require('querystring');
-var util = require('util');
 
 http.createServer(function(req, res) {
-    var post = 'testtttt'; //定义了一个post变量，用于暂存请求体的信息
-    req.on('data', function(chunk) { //通过req的data事件监听函数，每当接受到请求体的数据，就累加到post变量中
-        post += chunk;
+    //req.setEncoding('utf-8');
+    var postData = '';
+    // 注册监听, 接收数据块
+    req.on("data", function(postDataChunk) {
+        postData += postDataChunk;
     });
-    req.on('end', function() { //在end事件触发后，通过querystring.parse将post解析为真正的POST请求格式，然后向客户端返回。
-        post = querystring.parse(post);
-        res.end(util.inspect(post));
+    // 数据接收完毕, 执行回调函数
+    req.on("end", function() {
+        var result = querystring.parse(postData); //解析 HEADER 中的数据
+        
+        res.writeHead(200, { 'Content-Type': 'text/plain' });
+        res.end(result);
     });
 }).listen(3000);
