c6e9db5f59c03e6bf933e6c4b565029e940110ac
Ding Songwen <DingSoung@gmail.com>
2017-10-09 21:25:40 +0800

    no message

---
+++ weapp/pages/home/index.js
@@ -0,0 +1,10 @@
+var WxParse = require('../../wxParse/wxParse.js');
+
+Page({
+  data: {
+  },
+  onLoad: function () {
+    var that = this;
+    //WxParse.wxParse('article', 'html', article, that, 5);
+  }
+})
